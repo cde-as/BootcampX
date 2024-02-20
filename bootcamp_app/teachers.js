bab4dd53e5c51818119097cf2a49627f0588a259
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-02-20 02:31:50 -0800

    Change to Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,20 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-pool
-  .query(
-    `
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students on student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${cohortName}%'
-`
-  )
+`;
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((row) => {
       console.log(
@@ -27,10 +30,3 @@ WHERE cohorts.name LIKE '%${cohortName}%'
     });
   })
   .catch((err) => console.error("query error", err.stack));
-
-
-/*   INSERT INTO assistance_requests (id, student_id, teacher_id, assignment_id, created_at, started_at, completed_at, student_feedback, teacher_feedback)
-
-  INSERT INTO cohorts (id, name, start_date, end_date) VALUES (1, 'FEB12', '2018-02-12T08:00:00.000Z', '2018-04-20T07:00:00.000Z');
-
-  INSERT INTO teachers (id, name, is_active, start_date, end_date) VALUES (1, 'Helmer Rodriguez', true, '2018-02-12T08:00:00.000Z', null); */
